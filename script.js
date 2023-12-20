a76cf9cd9590a32a68f11577da693710ec643965
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-20 14:55:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+	let form = document.getElementById('form1');
+let firstInput = form.elements('fname');
+	let lastInput = form.elemenrs('lname');
 
+
+	let firstName = firstInput.value;
+	let lastName = lastInput.value;
+
+	alert("First name: " + firstName + "\nLast name: " + lastName);
+	
 }
